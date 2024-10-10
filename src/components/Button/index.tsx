c2fd1bd078bba61aa17b42c2fe698c6bee2b87c1
panggih mario <panggihmario@gmail.com>
2024-10-10 20:41:28 +0700

    commit

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef, ReactNode, forwardRef } from "react"
+import { ComponentPropsWithoutRef, ReactNode } from "react"
 import { cn } from "../../lib/utils"
 import { cva, type VariantProps } from 'class-variance-authority';
 type ButtonProps = {
